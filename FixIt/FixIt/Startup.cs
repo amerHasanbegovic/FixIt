5ca52e4482156339bdfb53c15ff00883963c23af
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-12 02:48:20 +0200

    configured swagger in startup class

--- FixIt/FixIt/Startup.cs
+++ FixIt/FixIt/Startup.cs
@@ -26,6 +26,7 @@ namespace FixIt
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,16 @@ namespace FixIt
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
