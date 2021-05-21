7480f760b50bf891e7e62953d0e08206891fca73
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-21 03:22:15 +0200

    models update, removed virtual attributes

--- FixIt/FixIt.Data/Models/Job.cs
+++ FixIt/FixIt.Data/Models/Job.cs
@@ -3,7 +3,7 @@
     public class Job
     {
         public int Id { get; set; }
-        public virtual Employee Employee { get; set; }
+        public Employee Employee { get; set; }
         public ServiceRequest ServiceRequest { get; set; }
         public JobDetail JobDetails { get; set; }
         public JobStatus Status { get; set; }
--- FixIt/FixIt.Data/Models/ServiceRating.cs
+++ FixIt/FixIt.Data/Models/ServiceRating.cs
@@ -5,8 +5,8 @@ namespace FixIt.Data.Models
     public class ServiceRating
     {
         public int Id { get; set; }
-        public virtual Service Service { get; set; }
-        public virtual User User { get; set; }
+        public Service Service { get; set; }
+        public User User { get; set; }
         public int Rating { get; set; }
         public DateTime RatingDate { get; set; }
     }
