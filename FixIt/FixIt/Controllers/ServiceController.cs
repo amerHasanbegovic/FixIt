7311c2c4362f5fcc076ce4489ab7fc9af21cf1b5
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-24 19:08:23 +0200

    renamed ServiceController

---
+++ FixIt/FixIt/Controllers/ServiceController.cs
@@ -0,0 +1,12 @@
+﻿using FixIt.Models.ViewModels.Service;
+using FixIt.Services.Interfaces;
+
+namespace FixIt.Controllers
+{
+    public class ServiceController : BaseController<ServiceViewModel>
+    {
+        public ServiceController(IBaseCRUDService<ServiceViewModel> service) : base(service)
+        {
+        }
+    }
+}
