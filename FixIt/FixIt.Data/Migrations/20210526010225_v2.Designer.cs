d1e476729aad0f769a1384897aa5305476cfebf0
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-26 03:05:03 +0200

    migration v2

--- FixIt/FixIt.Data/Migrations/20210526010225_v2.Designer.cs
+++ FixIt/FixIt.Data/Migrations/20210526010225_v2.Designer.cs
@@ -4,14 +4,16 @@ using FixIt.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FixIt.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210526010225_v2")]
+    partial class v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
