eabff56568e7e1f23ab8f874b107dfe819eca4f9
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-27 00:08:05 +0200

    fixed typo in controller name

---
+++ FixIt/FixIt/Controllers/ServiceTypeController.cs
@@ -0,0 +1,12 @@
+﻿using FixIt.Models.Models.ServiceType;
+using FixIt.Services.Interfaces;
+
+namespace FixIt.Controllers
+{
+    public class ServiceTypeController : BaseController<ServiceTypeModel, ServiceTypeModel, ServiceTypeModel>
+    {
+        public ServiceTypeController(IBaseCRUDService<ServiceTypeModel, ServiceTypeModel, ServiceTypeModel> service) : base(service)
+        {
+        }
+    }
+}
---
+++ FixIt/FixIt/Controllers/SexController.cs
@@ -0,0 +1,12 @@
+﻿using FixIt.Models.Models.Sex;
+using FixIt.Services.Interfaces;
+
+namespace FixIt.Controllers
+{
+    public class SexController : BaseController<SexViewModel, object, object>
+    {
+        public SexController(IBaseCRUDService<SexViewModel, object, object> service) : base(service)
+        {
+        }
+    }
+}
