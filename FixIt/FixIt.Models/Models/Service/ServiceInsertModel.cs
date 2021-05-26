50b0b023e0dfdd743d7f2f5f6a355a160e367494
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-26 20:13:57 +0200

    upadated service and serviceType insert models

--- FixIt/FixIt.Models/Models/Service/ServiceInsertModel.cs
+++ FixIt/FixIt.Models/Models/Service/ServiceInsertModel.cs
@@ -1,10 +1,18 @@
-﻿namespace FixIt.Models.Models.Service
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FixIt.Models.Models.Service
 {
     public class ServiceInsertModel
     {
+        [Required(ErrorMessage = "Service name is required!")]
         public string Name { get; set; }
+        
         public string Description { get; set; }
+        
+        [Required(ErrorMessage = "Service price is required!")]
         public double Price { get; set; }
+
+        [Required(ErrorMessage = "Type is required!")]
         public int ServiceTypeId { get; set; }
     }
 }
--- FixIt/FixIt.Models/Models/ServiceType/ServiceTypeModel.cs
+++ FixIt/FixIt.Models/Models/ServiceType/ServiceTypeModel.cs
@@ -1,7 +1,10 @@
-﻿namespace FixIt.Models.Models.ServiceType
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FixIt.Models.Models.ServiceType
 {
     public class ServiceTypeModel
     {
+        [Required(ErrorMessage = "Service type name is required!")]
         public string Name { get; set; }
     }
 }
