c4c748819c73346bde0a66e7f66c643288152882
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-27 22:37:31 +0200

    registered new service

--- FixIt/FixIt/Startup.cs
+++ FixIt/FixIt/Startup.cs
@@ -19,6 +19,7 @@ using FixIt.Models.Models.PaymentType;
 using FixIt.Models.Models.JobStatus;
 using FixIt.Models.Models.ServiceRating;
 using FixIt.Models.Models.Payment;
+using FixIt.Models.Models.ServiceRequest;
 
 namespace FixIt
 {
@@ -50,6 +51,7 @@ namespace FixIt
             services.AddScoped<IBaseCRUDService<JobStatusViewModel, object, object>, BaseCRUDService<JobStatus, JobStatusViewModel, object, object>>();
             services.AddScoped<IBaseCRUDService<ServiceRatingViewModel, ServiceRatingInsertModel, ServiceRatingUpdateModel>, BaseCRUDService<ServiceRating, ServiceRatingViewModel, ServiceRatingInsertModel, ServiceRatingUpdateModel>>();
             services.AddScoped<IBaseCRUDService<PaymentViewModel, PaymentInsertModel, object>, BaseCRUDService<Payment, PaymentViewModel, PaymentInsertModel, object>>();
+            services.AddScoped<IBaseCRUDService<ServiceRequestViewModel, ServiceRequestInsertModel, object>, BaseCRUDService<ServiceRequest, ServiceRequestViewModel, ServiceRequestInsertModel, object>>();
 
             services.AddScoped<IEmployeeService, EmployeeService>();
             services.AddScoped<IServiceService, ServiceService>();
