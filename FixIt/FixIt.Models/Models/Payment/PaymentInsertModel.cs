4b49bdef811b8032971751be7da6090582f7d5ea
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-27 21:39:06 +0200

    updated PaymentInsert and PaymentView model

--- FixIt/FixIt.Models/Models/Payment/PaymentInsertModel.cs
+++ FixIt/FixIt.Models/Models/Payment/PaymentInsertModel.cs
@@ -1,6 +1,19 @@
-﻿namespace FixIt.Models.Models.Payment
+﻿using System;
+
+namespace FixIt.Models.Models.Payment
 {
     public class PaymentInsertModel
     {
+        public int Id { get; set; }
+        public double Total { get; set; }
+        public int PaymentTypeId { get; set; }
+#nullable enable
+        public string? FullName { get; set; }
+#nullable enable
+        public string? CreditCardNumber { get; set; }
+#nullable enable
+        public string? CVV { get; set; }
+#nullable enable
+        public DateTime PaymentDate { get; set; }
     }
 }
