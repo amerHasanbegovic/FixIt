c87b3604e13c371516c8953b73b7bac0d8f793b3
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-30 00:46:24 +0200

    updated ServiceInsertModel

--- FixIt/FixIt.Models/Models/Service/ServiceInsertModel.cs
+++ FixIt/FixIt.Models/Models/Service/ServiceInsertModel.cs
@@ -1,18 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace FixIt.Models.Models.Service
+﻿namespace FixIt.Models.Models.Service
 {
     public class ServiceInsertModel
     {
-        [Required(ErrorMessage = "Service name is required!")]
         public string Name { get; set; }
-        
         public string Description { get; set; }
-        
-        [Required(ErrorMessage = "Service price is required!")]
         public double Price { get; set; }
-
-        [Required(ErrorMessage = "Type is required!")]
         public int ServiceTypeId { get; set; }
     }
 }
