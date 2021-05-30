966e2e66c9b035b86b600038ae334fb8643f648d
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-30 02:45:45 +0200

    added user roles, register, login and response models

---
+++ FixIt/FixIt.Data/Models/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace FixIt.Data.Models
+{
+    public static class UserRoles
+    {
+        public const string admin = "admin";
+        public const string user = "user";
+    }
+}
---
+++ FixIt/FixIt.Models/Models/Auth/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FixIt.Models.Models.Auth
+{
+    public class ResponseModel
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
