81e46a6d91c11cae7c27c92f6b76478183bd940e
amer.hasanbegovic@hotmail.com <amer.hasanbegovic@hotmail.com>
2021-05-31 19:58:59 +0200

    updated user update model

--- FixIt/FixIt.Models/Models/User/UserUpdateModel.cs
+++ FixIt/FixIt.Models/Models/User/UserUpdateModel.cs
@@ -1,8 +1,13 @@
-﻿namespace FixIt.Models.Models.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FixIt.Models.Models.User
 {
     public class UserUpdateModel
     {
+        [Required(ErrorMessage = "First name is required!")]
         public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Last name is required!")]
         public string Lastname { get; set; }
         public byte[] Photo { get; set; }
         public string Address { get; set; }
