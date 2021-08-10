58c27c1cadc37b4eb3c2a5167adebf3fd8414245
Amer Hasanbegovic <amer.hasanbegovic@hotmail.com>
2021-08-11 01:15:54 +0200

    styling fix ServicePage

--- FixIt/FixIt.Mobile/FixIt.Mobile/Views/ServicesPage.xaml.cs
+++ FixIt/FixIt.Mobile/FixIt.Mobile/Views/ServicesPage.xaml.cs
@@ -40,5 +40,10 @@ namespace FixIt.Mobile.Views
             };
             ServicesListView.ItemsSource = await _apiService.Get<IEnumerable<ServiceViewModel>>(model);
         }
+
+        private void ServicesListView_Focused(object sender, FocusEventArgs e)
+        {
+
+        }
     }
 }
