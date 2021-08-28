4c9018e85d49cfaa43b33bf8948f9685806dbbb3
Amer Hasanbegovic <amer.hasanbegovic@hotmail.com>
2021-08-28 15:44:52 +0200

    implemented docker support

---
+++ FixIt/FixIt/SetupService.cs
@@ -0,0 +1,17 @@
+﻿using FixIt.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FixIt
+{
+    public class SetupService
+    {
+        public static void Init(ApplicationDbContext context)
+        {
+            context.Database.Migrate();
+        }
+    }
+}
